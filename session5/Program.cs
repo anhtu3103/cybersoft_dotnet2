855e1e1347847e60cd3dbb609ef0dd2972961d86
anhtu3103 <lqanhtu@gmail.com>
2024-11-05 23:28:01 +0700

    Push code

---
+++ session5/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int result = tinhTong(5, 10);
+        Console.WriteLine($"Tổng 2 số là {result}");
+    }
+
+    private static int tinhTong(int a, int b)
+    {
+        return a + b;
+    }
+}
--- session8/Program.cs
+++ session8/Program.cs
@@ -1,4 +1,6 @@
 ﻿
+using System;
+
 int[] IstNumber = new int[12]{20, 81, 97, 63, 72, 11, 20, 15, 33, 15, 41, 20};
 int total = 0;
 for(int i = 0; i < IstNumber.Length; i++) 
@@ -6,4 +8,3 @@ for(int i = 0; i < IstNumber.Length; i++)
     total += IstNumber[i];
 }
 Console.WriteLine($"{total}");
-
